c5aa5b2d20e7f0cc11b8f76d2e2fac85aecb5159
Julian Stone <julian.stone@students.olin.edu>
2020-02-25 15:10:21 -0500

    Starting error handling

--- board.c
+++ board.c
@@ -64,26 +64,40 @@ void add_ship(int *board, int rows, int cols, int ship, int sX, int sY, int dir)
   }
 }
 
+int getDir(char * input) {
+  printf("Direction? Right (1) or Down (2): ");
+  fgets(input, 5, stdin);
+  int dir = atoi(input);
+  while(dir != 1 && dir != 2) {
+    printf("Invalid input\n");
+    printf("Direction? Right (1) or Down (2): ");
+    fgets(input, 5, stdin);
+    dir = atoi(input);
+  }
+  return dir;
+}
+
+void getStart(int * s, char * input, int dir, int ship_type, int rows, int cols) {
+  printf("Starting Coordinate? Options A1-K9: ");
+  fgets(input, 5, stdin);
+  s[0] = input[0]-'A';
+  s[1] = input[1]-'1';
+  int right = (dir == 1) ? 1 : 0;
+  int down = (dir == 2) ? 1 : 0;
+  int valX = (s[0] < rows - right*ship_type);
+}
+
 void build_board(int *board, int rows, int cols) {
+  make_board(board,rows,cols);
   int i;
   char input[5];
   for(i = 1; i <= 5; i++) {
     int ship_type = get_type(i);
     printf("Let's place Ship %d of size %d...\n", i, ship_type);
-    printf("Direction? Right (1) or Down (2): ");
-    fgets(input, 5, stdin);
-    int dir = atoi(input);
-    while(dir != 1 && dir != 2) {
-      printf("Invalid input\n");
-      printf("Direction? Right (1) or Down (2): ");
-      fgets(input, 5, stdin);
-      dir = atoi(input);
-    }
-    printf("Starting Coordinate? Options A1-K9: ");
-    fgets(input, 5, stdin);
-    int sX = input[0]-65;
-    int sY = input[1]-'0'-1;
-    add_ship(board,rows,cols,i,sX,sY,dir);
+    int dir = getDir(input);
+    int s[2];
+    getStart(s,input, dir, ship_type, rows, cols);
+    add_ship(board,rows,cols,i,s[0],s[1],dir);
     printf("\n");
     print_board(board,rows,cols);
     printf("\n");
@@ -97,7 +111,6 @@ order to create and print a board.
 int main() {
   int rows = 9, cols = 11;
   int *board = (int *)malloc(rows*cols*sizeof(int));
-  make_board(board, rows, cols);
   build_board(board, rows, cols);
   print_board(board, rows, cols);
   return 0;
